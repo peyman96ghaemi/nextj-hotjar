4d5fd2e6b30ce24d47001c96c4a7a9cd9f0e29d6
peymn88gh <peymn88gh@gmail.com>
2024-05-22 23:57:18 +0330

    fix: script build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,18 +18,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script strategy="lazyOnload">
-          {`
-        (function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:4993672,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
-      `}
-        </Script>
+        <Script
+          id="show-banner"
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:4993672,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
+          }}
+        ></Script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
