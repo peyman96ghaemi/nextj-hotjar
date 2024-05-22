693f4d5325ee7e857979bfe994304de875843d53
peymn88gh <peymn88gh@gmail.com>
2024-05-23 01:22:25 +0330

    fix: seventh try

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,17 +20,17 @@ export default function RootLayout({
       <body className={inter.className}>{children}
       <Script
           id="show-banner"
-          dangerouslySetInnerHTML={{
-            __html: `(function(h,o,t,j,a,r){
+          >
+         {`(function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
               h._hjSettings={hjid:4993672,hjsv:6};
               a=o.getElementsByTagName('head')[0];
               r=o.createElement('script');r.async=1;
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
               a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`,
-          }}
-        ></Script>
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+          }
+        </Script>
       </body>
     </html>
   );
